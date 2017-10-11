ead97f21f69c5d014b784d9fb264d9ebe9e8427c
Trishul <trishulgoel@inlaptop44.io-media.com>
2017-10-11 12:46:15 +0530

    map revamped

---
+++ data/mentors.js
@@ -0,0 +1,16 @@
+module.exports.mentors = [
+		{
+			"name": "Santosh Viswanatham",
+			"position": [17.409703, 78.505197],
+			"emailId": "viswanathamsantosh@gmail.com",
+			"place": "Hyderabad, India",
+			"mozillians_url": "https://mozillians.org/en-US/u/isantoshv/"
+		},
+		{
+			"name": "Trishul Goel",
+			"position": [28.6129167, 77.227321],
+			"emailId": "trishul.goel@gmail.com",
+			"place": "New Delhi, India",
+			"mozillians_url": "https://mozillians.org/en-US/u/trishul/"
+		}
+];
---
+++ src/script.js
@@ -0,0 +1,50 @@
+import L from 'leaflet';
+import { mentors } from '../data/mentors';
+
+class mentorsMap {
+
+	constructor(props) {
+		this.mapElement = {};
+		this.map = {};
+		this.mapId = 'mentorsMap';
+		this.mapCenter = [51.505, -0.09];
+		this.mapZoom = 2;
+		this.mapTitleLayer = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	}
+
+	createMapElement() {
+		this.mapElement = document.createElement('div');
+		this.mapElement.setAttribute('id', this.mapId);
+		document.body.appendChild(this.mapElement);
+	}
+
+	drawMap() {
+		this.map = L.map(this.mapId, {
+		    center: this.mapCenter,
+		    zoom: this.mapZoom
+		});
+		L.tileLayer(this.mapTitleLayer).addTo(this.map);	
+	}
+
+	placeMarkers() {
+		mentors.forEach(mentor => {
+			const popupContent=`
+			<div class='avatar'></div>
+			<div>
+				<span><b>${mentor.name}</b><span>
+				<span>${mentor.place}<span>
+				<span><a href='mailto:${mentor.emailId}'>${mentor.emailId}</a></span>
+				<span><a href='${mentor.mozillians_url}' target='_blank'>${mentor.mozillians_url}</a></span>
+				
+			</div>`;
+			const marker = L.marker(mentor.position).addTo(this.map);
+
+			marker.bindPopup(popupContent);
+		});
+	}
+}
+
+const mapIt = new mentorsMap;
+mapIt.createMapElement();
+mapIt.drawMap();
+mapIt.placeMarkers();
